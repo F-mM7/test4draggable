34515205f985b76d18bff5edb8d7bee099396ab9
F <121377389+F-mM7@users.noreply.github.com>
2024-04-11 19:09:45 +0900

    [update]jqueryによるdom制御の記述とレイアウトの変更

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 $(function () {
   $(".piece").draggable({
-    // revert: true,
     scroll: false,
-    grid: [30, 30],
-    // snap: true,
-    // snapTolerance: 20,
+    grid: [width, width],
     containment: "#box",
   });
 });
+
+const N = 12;
+
+//append piece
+for (let i = 0; i < N; ++i)
+  $("<div>", { class: "piece", text: i + 1 }).appendTo("#box");
+
+//sizing
+let width = Math.floor(
+  Math.min($(window).width(), $(window).height()) / (N + 1)
+);
+$(".piece").css({ width: width, height: width, "font-size": width * 0.7 });
+$("#box").css({ width: width * N, height: width * N });
