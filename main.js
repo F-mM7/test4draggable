bd28c92b1f28db7ea17fd18094c1509234467cdb
F <121377389+F-mM7@users.noreply.github.com>
2024-04-11 19:21:30 +0900

    [test]スマホで描画サイズがおかしいので試験

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-$(function () {
-  $(".piece").draggable({
-    scroll: false,
-    grid: [width, width],
-    containment: "#box",
-  });
-});
-
 const N = 12;
 
 //append piece
 for (let i = 0; i < N; ++i)
   $("<div>", { class: "piece", text: i + 1 }).appendTo("#box");
 
-//sizing
-let width = Math.floor(
-  Math.min($(window).width(), $(window).height()) / (N + 1)
-);
-$(".piece").css({ width: width, height: width, "font-size": width * 0.7 });
-$("#box").css({ width: width * N, height: width * N });
+function resize() {
+  //sizing
+  let width = Math.floor(
+    Math.min($(window).width(), $(window).height()) / (N + 1)
+  );
+  $(".piece").css({ width: width, height: width, "font-size": width * 0.7 });
+  $("#box").css({ width: width * N, height: width * N });
+
+  $(".piece").draggable({
+    scroll: false,
+    grid: [width, width],
+    containment: "#box",
+  });
+}
+resize();
+
+// $(window).resize(resize);
